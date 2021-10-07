42ae4f2c772c0fee5c01ee1122532b5499d8fa52
Capstone <michael.rohweder1982@gmail.com>
2021-10-07 19:20:11 -0400

    yo

--- social_site/settings.py
+++ social_site/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = BASE_DIR / 'static/'
 
 STATICFILES_DIRS = [
     #BASE_DIR / 'static',
-    BASE_DIR / 'post' / 'static',
+    BASE_DIR / 'posts' / 'static',
     BASE_DIR / 'profiles' / 'static',
 ]
 
