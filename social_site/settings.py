1986d6992c94806e0fc19be5f0d3e4c8f8b17cf3
Capstone <michael.rohweder1982@gmail.com>
2021-10-07 19:16:11 -0400

    fixed settings.py

--- social_site/settings.py
+++ social_site/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / '/static/'
 
 STATICFILES_DIRS = [
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'profiles' / 'static',
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
