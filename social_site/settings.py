18ab18c10127098d53ba28fd315fbc51586f79f6
Capstone <michael.rohweder1982@gmail.com>
2021-10-07 20:40:36 -0400

    blah blah

--- social_site/settings.py
+++ social_site/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 
-import django_heroku
+django_heroku.settings(locals())
 
