f0c485e5c3a5099a16a595ba5b6d95e6e36f26d4
Capstone <michael.rohweder1982@gmail.com>
2021-10-12 22:37:57 -0400

    secret key

--- social_site/settings.py
+++ social_site/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('secret_key')
+SECRET_KEY = 'django-insecure-)u*0s21x+=2g22zzqlme36himm@!mhw^qdk9l-q+(#jr2qokiv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
